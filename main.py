a9a72d9ab89172dede6e4fbc2255c64c29c9dc71
kleinjb25 <kleinjb@miamioh.edu>
2024-02-17 16:52:50 -0500

    put code in try catch for safer handling, simplified time zone conversion lines into 1 line

--- main.py
+++ main.py
@@ -14,22 +14,25 @@ load_dotenv()
 discord_token = os.getenv('DISCORD_TOKEN')
 
 def getMessages() :
-	file = f"{date.today()}.txt"
-	headers = {'authorization': discord_token}
-	# If someone deletes the attendance channel accidentally,
-	# just make sure you have developer tools enabled in the Discord Settings,
-	# then right click the new attendance channel -> copy id and paste it in between "channels/channelID/messages".
-	# In this case, 1203908563374706780 is the current channelID.
-	req = requests.get(f'https://discord.com/api/v9/channels/1203908563374706780/messages', headers=headers)
-	jsonData = json.loads(req.text)
-	with open(file, 'a') as file:
-		for message in jsonData:
-			currentTimeStamp = datetime.fromisoformat(message.get('timestamp', '')[:-6]) # remove the last 6 characters (timezone info)
-			currentTimeStamp = currentTimeStamp.replace(tzinfo=pytz.timezone('GMT'))  # Set timezone to GMT
-			currentTimeStamp = currentTimeStamp.astimezone(pytz.timezone('US/Eastern'))
-			# This checks only for messages within the last 24 hours of running the script.
-			# If you try to run the script more than 24 hours after the meeting,
-			# it might skip messages sent near the beginning of the meeting.
-			if datetime.now(tz=pytz.timezone('US/Eastern')) - currentTimeStamp < timedelta(hours=24) and " " not in message['content']:
-				file.write(message['content'] + "@miamioh.edu\n")
+	try:
+		file = f"{date.today()}.txt"
+		headers = {'authorization': discord_token}
+		# If someone deletes the attendance channel accidentally,
+		# just make sure you have developer tools enabled in the Discord Settings,
+		# then right click the new attendance channel -> copy id and paste it in between "channels/channelID/messages".
+		# In this case, 1203908563374706780 is the current channelID.
+		req = requests.get(f'https://discord.com/api/v9/channels/1203908563374706780/messages', headers=headers)
+		jsonData = json.loads(req.text)
+		with open(file, 'a') as file:
+			for message in jsonData:
+				# Get the time the message was sent and convert it to EST
+				currentTimeStamp = datetime.fromisoformat(message.get('timestamp', '')[:-6]).replace(tzinfo=pytz.timezone('GMT')).astimezone(pytz.timezone('US/Eastern'))
+				# This checks only for messages within the last 24 hours of running the script.
+				# If you try to run the script more than 24 hours after the meeting,
+				# it might skip messages sent near the beginning of the meeting.
+				if datetime.now(tz=pytz.timezone('US/Eastern')) - currentTimeStamp < timedelta(hours=24) and " " not in message['content']:
+					file.write(message['content'] + "@miamioh.edu\n")
+	except Exception as e:
+		print(e)
+		input()
 getMessages()
