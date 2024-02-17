bb6bbf69aeca78aab6f9459e36d61f73ace4c22d
kleinjb25 <kleinjb@miamioh.edu>
2024-02-17 12:53:44 -0500

    fixed time zone issue and made it so it doesn't add messages with spaces

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ It will then upload all of the messages to a text file to allow for easy attenda
 """
 import json
 import os
+import pytz
 import requests
 from datetime import datetime, date, timedelta
 # Used to load the environment variables in .env
@@ -21,15 +22,14 @@ def getMessages() :
 	# In this case, 1203908563374706780 is the current channelID.
 	req = requests.get(f'https://discord.com/api/v9/channels/1203908563374706780/messages', headers=headers)
 	jsonData = json.loads(req.text)
-	prevTimeStamp = datetime.now()
 	with open(file, 'a') as file:
 		for message in jsonData:
 			currentTimeStamp = datetime.fromisoformat(message.get('timestamp', '')[:-6]) # remove the last 6 characters (timezone info)
-			currentTimeStamp = currentTimeStamp.replace(tzinfo=None) # ignore timezone
-			# This checks only for messages within the last 6 hours (21600 seconds) of running the script.
-			# If you try to run the script more than 2 hours after the meeting,
+			currentTimeStamp = currentTimeStamp.replace(tzinfo=pytz.timezone('GMT'))  # Set timezone to GMT
+			currentTimeStamp = currentTimeStamp.astimezone(pytz.timezone('US/Eastern'))
+			# This checks only for messages within the last 24 hours of running the script.
+			# If you try to run the script more than 24 hours after the meeting,
 			# it might skip messages sent near the beginning of the meeting.
-			if prevTimeStamp - currentTimeStamp < timedelta(seconds=21600):
+			if datetime.now(tz=pytz.timezone('US/Eastern')) - currentTimeStamp < timedelta(hours=24) and " " not in message['content']:
 				file.write(message['content'] + "@miamioh.edu\n")
-			prevTimeStamp = currentTimeStamp
 getMessages()
